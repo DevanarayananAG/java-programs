414fc3d70b777fd4dfd17e8d188fcb17fde786f1
DevanarayananAG <144927687+DevanarayananAG@users.noreply.github.com>
2024-08-17 23:26:16 +0530

    Add files via upload
    java program to find the count of vowels in a string

---
+++ vowel.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class vowel
+{
+	public static void main(String[] args)
+	{ 
+	        int count=0,i;
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the word to be checked \n");
+		
+		String str= s.nextLine();
+		str=str.toLowerCase();
+		
+		for(i=0;i<=str.length();i++)
+		{
+			
+			if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u')
+			{count++;}
+		
+		}
+	System.out.println("the number of vowels is : " +count);		
+	}
+}
